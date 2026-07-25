dd14b48d3a40dfa3ee56e29c05555be989955c1a
karan789023 <karanyadav73681@gmail.com>
2026-07-25 23:28:17 +0530

    Fix validateUrl import case

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://pulseiq-1-7to7.onrender.com/api/analyze",
+  baseURL: "https://pulseiq-1-7to7.onrender.com/api",
 });
 
 export const analyzeWebsite = async (url) => {
-  const { data } = await API.post("/", {
+  const { data } = await API.post("/analyze", {
     url,
   });
 
